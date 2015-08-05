a68f2332811b13411e0014e25ecc78ded2648246
liulinlin <liulinlin@zhumengyuan.com>
2015-08-05 16:10:50 +0800

    init project

---
+++ django_utils/forms.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+
+
+def validate_form(form_class, data):
+    form = form_class(data)
+    if form.is_valid():
+        return True, form.cleaned_data
+    errors = []
+    for key, field in form.declared_fields.items():
+        if field.required and key not in data:
+            errors.append({"field": key, "code": "missing_field"})
+        elif key in form.errors:
+            errors.append({"field": key, "code": "invalid"})
+    return False, errors
